9e0bd8eda72119f72bc989bb271fe1eef472e69d
Your Name <sanchikasingh65@gmail.com>
2026-04-08 12:57:18 +0530

    Added Maximum Subarray solution

---
+++ MaxSub.java
@@ -0,0 +1,17 @@
+public class MaxSub {
+
+     public int MaxSubArray(int[]nums){
+        int maxsum=nums[0];
+        int currsum=0;
+        for(int num:nums){
+            currsum+=num;
+            if(currsum>maxsum){
+                maxsum=currsum;
+            }
+            if(currsum<0){
+                currsum=0;
+            }
+        }
+        return maxsum;
+     }
+}
