67fc2db17fa3e26ed2481ecc28e696d5684d758c
Your Name <sanchikasingh65@gmail.com>
2026-04-09 00:50:35 +0530

    Added Minimum In Sorted Array solution

---
+++ MinInRSA.java
@@ -0,0 +1,15 @@
+public class MinInRSA {
+    public int MinInRotateSortArray(int []nums){
+        int l=0;int h=nums.length-1;
+        while(l<=h){
+            int mid=l+(h-l)/2;
+            if(nums[mid]>nums[h]){
+                l=mid+1;
+            }
+            else{
+                h=mid;
+            }
+        }
+        return nums[l];
+    }
+}
