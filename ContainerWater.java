3b6c6ddc1bae9ed7c5cdd9ac75e7fb3592673249
Your Name <sanchikasingh65@gmail.com>
2026-04-10 13:53:00 +0530

    Added Container with most  Water

---
+++ ContainerWater.java
@@ -0,0 +1,22 @@
+public class ContainerWater {
+
+    public int maxArea(int[] height){
+       int left=0;
+       int right=height.length-1;
+       int max=0;
+       while(left<right){
+        int h=Math.min(height[left],height[right]);
+        int width=right-left;
+        int area=h*width;
+        max=Math.max(max,area);
+        if(height[left]<height[right]){
+            left++;
+        }
+        else{
+            right--;
+        }
+
+       }
+       return max;
+    }
+}
