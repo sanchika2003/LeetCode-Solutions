a635130e72bc495113f88233e4c8797384db52e8
Your Name <sanchikasingh65@gmail.com>
2026-04-27 11:42:24 +0530

    Third max number

---
+++ Thirdmax.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int thirdMax(int[] nums) {
+        long first = Long.MIN_VALUE;
+        long second = Long.MIN_VALUE;
+        long third = Long.MIN_VALUE;
+
+        for (int num : nums) {
+            // skip duplicate values
+            if (num == first || num == second || num == third) {
+                continue;
+            }
+
+            if (num > first) {
+                third = second;
+                second = first;
+                first = num;
+            } else if (num > second) {
+                third = second;
+                second = num;
+            } else if (num > third) {
+                third = num;
+            }
+        }
+
+        // if we don't have 3 distinct elements, return the maximum
+        return third == Long.MIN_VALUE ? (int) first : (int) third;
+    }
+}
