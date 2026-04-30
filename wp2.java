89a46f4155d9b774b55c7b5c164b66ff515ad147
Your Name <sanchikasingh65@gmail.com>
2026-04-30 23:12:43 +0530

    GCD OF 3 NUMBERS

---
+++ wp2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Main 
+{
+    public static int gcd(int a,int b)
+    {
+        if(b==0)
+            return a;
+        return gcd(b,a%b);
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        System.out.println(gcd(a,gcd(b,c)));
+    }
+}
