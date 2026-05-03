074e545e2fdc94bfd44dad2a8386e7cc37e2cf5b
Your Name <sanchikasingh65@gmail.com>
2026-05-03 22:48:22 +0530

    Next Permutation

---
+++ Np.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+         int n = nums.length;
+        int i = n - 2;
+
+        // Step 1: Find the first decreasing element from the right
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+    }
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    }
